45e7b1e1563de1f09d6618ed6545828cb72c8ff0
Saurabh Salunke <119050714+saurabh-sss-555@users.noreply.github.com>
2022-11-27 13:59:05 +0530

    Add files via upload

---
+++ Splash.java
@@ -0,0 +1,34 @@
+package com.maj.gstbillingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent i=new Intent(getApplicationContext(),SetupPasswordActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        }).start();
+
+
+
+    }
+}
